ecb0dff9bf7ed70ea74442c10425f928d0e5dfb5
Marco Coccato <coccato.m@gmail.com>
2019-12-05 22:01:31 +0100

    added score system

---
+++ Assets/Scripts/ScoreChecker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreChecker : MonoBehaviour
+{
+    public enum Positions
+    {
+        Top, Net, Other
+    };
+
+    private bool    hasTopCollided = false,
+                    hasNetCollided = false,
+                    hasOtherscollided = false;
+
+    public void hasCollided(Positions p)
+    {
+        if (p == Positions.Top || hasTopCollided)
+        {
+            hasTopCollided = true;
+            if (p == Positions.Net)
+            {
+                hasNetCollided = true;
+            }
+        }
+
+        if(p == Positions.Other)
+        {
+            hasOtherscollided = true;
+        }
+
+        if(hasTopCollided && hasNetCollided)
+        {
+            //TODO SCORE
+            if(hasOtherscollided)
+            {
+                //min
+                Debug.Log("Min Score");
+            }
+            else
+            {
+                //perfect score
+                Debug.Log("Perfect Score");
+            }
+
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        hasNetCollided = false;
+        hasTopCollided = false;
+        hasOtherscollided = false;
+    }
+}
---
+++ Assets/Scripts/ScoreCheckerChild.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ScoreCheckerChild : MonoBehaviour
+{
+    [SerializeField]
+    private ScoreChecker.Positions position;
+
+    private ScoreChecker scoreChecker = null;
+
+    private void Start()
+    {
+        scoreChecker = GameObject.FindObjectOfType<ScoreChecker>();
+        if(! scoreChecker)
+        {
+            Debug.LogError("Score checker not found in children");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Ball"))
+        {
+            scoreChecker.hasCollided(position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            scoreChecker.hasCollided(position);
+        }
+    }
+}
