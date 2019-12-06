d995a1ba832865892568b1e86f8ff9713593f216
Marco Coccato <coccato.m@gmail.com>
2019-12-06 21:51:44 +0100

    Calculated direction fo throwing ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,18 +6,38 @@ public class BallController : MonoBehaviour
 {
     [SerializeField]
     private float   yMultiplier = 5f,
-                    zMultiplier = 5f;
+                    xZMultiplier = 5f;
+
     private Rigidbody myRigidbody;
+    private Transform target;
+    private Vector3 throwDirection;
+    private Vector3 localStartPosition;
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Target").transform;
+        CalculateThrowDirection();
+        localStartPosition = transform.localPosition;
+    }
+
+    private void CalculateThrowDirection()
+    {
+        throwDirection = Vector3.ProjectOnPlane(target.position - transform.position, Vector3.up);
+        throwDirection.Normalize();
+    }
+
+    public void ResetPosition()
+    {
+        transform.localPosition = localStartPosition;
+        myRigidbody.constraints = RigidbodyConstraints.FreezePosition;
+        CalculateThrowDirection();
     }
 
     public void ThrowBall(float power)
     {
         myRigidbody.constraints = RigidbodyConstraints.None;
-        Vector3 force = new Vector3(0f, yMultiplier, power * zMultiplier);
+        Vector3 force = throwDirection * power * xZMultiplier + Vector3.up * yMultiplier;
         myRigidbody.AddForce(force, ForceMode.Impulse);
     }
 }
