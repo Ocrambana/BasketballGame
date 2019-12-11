b2e82fecfaeffd1cc6d972f104dcabcb5a603f42
Marco Coccato <coccato.m@gmail.com>
2019-12-11 20:47:28 +0100

    Changed graphics settings and added camera flip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,11 +13,25 @@ public class Player : MonoBehaviour
 
     public void RepositionPlayerAndBall(Vector3 newPosition)
     {
+        if(Mathf.Sign(newPosition.x) != Mathf.Sign(transform.position.x))
+        {
+            FlipCamera();
+        }
+
         transform.position = newPosition;
         ball.ResetPosition();
 
         Vector3 toLookAt = Vector3.ProjectOnPlane(ball.TargetPosition, Vector3.up);
         transform.LookAt(toLookAt);
+    }
+
+    private void FlipCamera()
+    {
+        Vector3 cameraPos = Camera.main.transform.localPosition;
+        Camera.main.transform.localPosition = new Vector3(-cameraPos.x, cameraPos.y, cameraPos.z);
+
+        Quaternion cameraRot = Camera.main.transform.localRotation;
+        Camera.main.transform.localRotation = new Quaternion(cameraRot.x,-cameraRot.y, cameraRot.z, cameraRot.w);
     }
 
     public void ThrowBall(Vector3 power)
